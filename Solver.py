835235ba43dc46a94d0c6e8e456d51a7310dca5d
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2023-01-23 23:22:16 +0100

    m

--- Solver.py
+++ Solver.py
@@ -4,18 +4,24 @@ import collections
 import time
 from tkinter import *
 
+
 class Node:
 
     def __init__(self, puzzle, parent=None, action=None):
         self.puzzle = puzzle
         self.parent = parent
         self.action = action
+        if parent:
+            self.g = parent.g + 1
+        else:
+            self.g = 0
+        self.h = puzzle.h2()
 
     @property
     def state(self):
-        return str(self)
+        return str(self.puzzle)
 
-    @property 
+    @property
     def path(self):
         node, p = self, []
         while node:
@@ -32,73 +38,115 @@ class Node:
         return self.puzzle.actions
 
     def __str__(self):
-        return str(self.puzzle)
+        return str(self.puzzle) + ' ,g:' + str(self.g) + ' ,h:' + str(self.h)
 
+    def f(self):
+        return self.g + self.h
 
 
 class Solver:
 
-    def __init__(self, start ,fenetre):
+    def __init__(self, start, fenetre):
         self.start = start
         self.fenetre = fenetre
 
-    def solve(self):
-        print("Recherche de solution")
-        print(self.start.board)
-        global j
+    def solve_largeur(self):
+        print("Recherche de solution en largeur")
+        start_time = time.perf_counter()
         queue = collections.deque([Node(self.start)])
-        seen  = set()
+        seen = set()
         seen.add(queue[0].state)
         while queue:
             node = queue.pop()
             if node.solved:
-            	z= list(node.path)
-            	self.aff5(z)
-            	print("solution trouvée en", len(z) , " coups")
-            	break
+                end_time = time.perf_counter()
+                z = list(node.path)
+                print("solution trouvée en", len(z) - 1, " coups")
+                print("temps de resolution ", end_time-start_time, " s")
+                print("nombre de noeds visitees ", len(seen))
+                return z
             for move, action in node.actions:
-                child = Node(move(), node, action)             
+                child = Node(move(), node, action)
                 if child.state not in seen:
                     queue.appendleft(child)
                     seen.add(child.state)
 
-    def solve_Long(self):
-        global j
-        print("Recherche de solution 2")
+    def solve_profondeur(self):
+        print("Recherche de solution en profondeur")
+        start_time = time.perf_counter()
         queue = collections.deque([Node(self.start)])
-        seen  = set()
+        seen = set()
         seen.add(queue[0].state)
         while queue:
             node = queue.pop()
             if node.solved:
-            	z= list(node.path)
-            	self.aff5(z)
-            	print("solution trouvée en", len(z) , " coups")
-            	break
+                end_time = time.perf_counter()
+                z = list(node.path)
+                print("solution trouvée en", len(z) - 1, " coups")
+                print("temps de resolution ", end_time-start_time, " s")
+                print("nombre de noeds visitees ", len(seen))
+                return z
+            if node.g < 50:
+                for move, action in node.actions:
+                    child = Node(move(), node, action)
+                    if child.state not in seen:
+                        queue.append(child)
+                        seen.add(child.state)
+            
+                
+
+    def solve_a(self):
+        print("Recherche de solution en a*")
+        start_time = time.perf_counter()
+        ouvertSet = set([Node(self.start).state])
+        ouvert = [Node(self.start)]
+        fermeSet = set()
+        ferme = []
+        while len(ouvert) > 0:
+            node = min(ouvert,key= lambda e: e.f())
+            if node.solved:
+                end_time = time.perf_counter()
+                z = list(node.path)
+                print("solution trouvée en", len(z) - 1, " coups")
+                print("temps de resolution ", end_time - start_time, " s")
+                print("nombre de noeds visitees ", len(ferme))
+                return z
+            ouvert.remove(node)
+            ferme.append(node)
+            fermeSet.add(node.state)
             for move, action in node.actions:
                 child = Node(move(), node, action)
-                if child.state not in seen:
-                    queue.append(child)
-                    seen.add(child.state)
-        
-    def aff5(self , p , i=1):
-    	node = p[0]
-    	p=p[1:]
-    	x=node.puzzle.convL()
-    	print("coup",i," : ",x)
-    	node.puzzle.afficher2(x)
-    	if p:
-            self.fenetre.after(1500, self.aff5, p, i+1)
-    	else :
-        	print("fin")
+                if child.state in ouvertSet:
+                    for i in ouvert:
+                        if(i.state == child.state and i.g< child.g):
+                            i.parent = node
+                            i.g = child.g
+                            break
+                elif child.state in fermeSet:
+                    continue
+                ouvert.append(child)
+                ouvertSet.add(child.state)
+                
+        print("not found")
+
+    def aff5(self, p, i=1):
+        node = p[0]
+        p = p[1:]
+        x = node.puzzle.convL()
+        print("coup", i, " : ", x)
+        node.puzzle.afficher2(x)
+        if p:
+            self.fenetre.after(200, self.aff5, p, i + 1)
+        else:
+            print("fin")
 
 
 class Puzzle:
 
-    def __init__(self, board , root , Lph):
-        self.width = len(board[0]) 
+    def __init__(self, board, root, Lph):
+        self.width = len(board[0])
         self.board = board
-        #self.top = tkinter.Toplevel(root)
+        # self.top = tkinter.Toplevel(root)
         self.can = root
         self.Lph = Lph
 
@@ -107,17 +155,16 @@ class Puzzle:
 
         tab = []
         sol = True
-        for i in range (self.width):
-	        tab.extend(self.board[i]);
-
-        for j in range (len(tab)-2):
-        	if (tab[j]!=(tab[j+1]-1)):
-        		sol = False;
-        if tab[-1] != 0 :
-        	sol = False;
+        for i in range(self.width):
+            tab.extend(self.board[i])
+        for j in range(len(tab) - 2):
+            if tab[j] != (tab[j + 1] - 1):
+                return False
+        if tab[-1] != 0:
+            sol = False
         return sol
 
-    @property 
+    @property
     def actions(self):
         def create_move(at, to):
             return lambda: self.move(at, to)
@@ -125,23 +172,23 @@ class Puzzle:
         moves = []
         for i, j in itertools.product(range(self.width),
                                       range(self.width)):
-            direcs = {'R':(i, j-1),
-                      'L':(i, j+1),
-                      'D':(i-1, j),
-                      'U':(i+1, j)}
+            direcs = {'R': (i, j - 1),
+                      'L': (i, j + 1),
+                      'D': (i - 1, j),
+                      'U': (i + 1, j)}
 
             for action, (r, c) in direcs.items():
                 if r >= 0 and c >= 0 and r < self.width and c < self.width and \
-                   self.board[r][c] == 0:
-                    move = create_move((i,j), (r,c)), action
+                        self.board[r][c] == 0:
+                    move = create_move((i, j), (r, c)), action
                     moves.append(move)
         return moves
 
     def shuffle(self):
         puzzle = self
-        for k in range(1000):
+        for k in range(50):
             puzzle = random.choice(puzzle.actions)[0]()
-        x=puzzle.convL()
+        x = puzzle.convL()
         print(x)
         self.afficher2(x)
         self = puzzle
@@ -152,7 +199,7 @@ class Puzzle:
         board = []
         for row in self.board:
             board.append([x for x in row])
-        return Puzzle(board,self.can,self.Lph)
+        return Puzzle(board, self.can, self.Lph)
 
     def move(self, at, to):
         copy = self.copy()
@@ -161,11 +208,13 @@ class Puzzle:
         copy.board[i][j], copy.board[r][c] = copy.board[r][c], copy.board[i][j]
         return copy
 
-    def afficher2 (self,liste1):
+    def afficher2(self, liste1):
         "afficher les images sur le canvas"
-        for k in range(len(liste1)) :
-            eff =self.can.create_image((30+ 150*(k % self.width)), 30+(150*( k // self.width)), anchor=NW, image=self.Lph[0])
-            aff =self.can.create_image((30+ 150*(k % self.width)), 30+(150*( k // self.width)), anchor=NW ,image = self.Lph[liste1[k]])
+        for k in range(len(liste1)):
+            eff = self.can.create_image((30 + 150 * (k % self.width)), 30 + (150 * (k // self.width)), anchor=NW,
+                                        image=self.Lph[0])
+            aff = self.can.create_image((30 + 150 * (k % self.width)), 30 + (150 * (k // self.width)), anchor=NW,
+                                        image=self.Lph[liste1[k]])
 
     def pprint(self):
         for row in self.board:
@@ -173,10 +222,36 @@ class Puzzle:
         print()
 
     def convL(self):
-    	L=[]
-    	for row in self.board:
-    		L.extend(row)
-    	return L 
+        L = []
+        for row in self.board:
+            L.extend(row)
+        return L
+
+    def h(self):
+        h = -1
+        i = 0
+        for ligne in self.board:
+            j = 1
+            for case in ligne:
+                if (i * self.width + j) != case:
+                    h += 1
+                j += 1
+            i += 1
+        return h
+
+    def h2(self):
+        h = 0
+        i = 0
+        for ligne in self.board:
+            j = 0
+            for case in ligne:
+                x = (case - 1) // self.width
+                y = (case - 1) % self.width
+                if ((i != x) or j != y) and case != 0:
+                    h += abs(i - x) + abs(j - y)
+                j += 1
+            i += 1
+        return h
 
     def __str__(self):
         return ''.join(map(str, self))
@@ -184,4 +259,3 @@ class Puzzle:
     def __iter__(self):
         for row in self.board:
             yield from row
-
--- Taquin_main.py
+++ Taquin_main.py
@@ -1,88 +1,50 @@
-from tkinter import * 
-from Solver import Solver
-from Solver import Node
+from tkinter import *
 from Solver import Puzzle
-import time
+from Solver import Solver
 
-global puzzl , t ,j
-
-j=0
 fenetre = Tk()
+board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+photos = []
+for i in range(0, 16):
+    photos.append(PhotoImage(file="./images/" + str(i) + ".png"))
+Lph = photos[0:9]
+can = Canvas(width=180 * 3, height=180 * 3, bg='white')
+can.pack(side=TOP, padx=20, pady=20)
+fenetre['bg'] = 'white'
+fenetre.title('Tp AI')
+puzzle = Puzzle(board, can, Lph)
 
-
-print("Donner la taille de votre puzzle : \n 3 --> 3*3 \n 4 --> 4*4")
-t=int(input())
-time.sleep(2)
-
-if(t==3):
-	board = [[1,2,3],[4,5,6],[7,8,0]]
-else :
-	board = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
-
-photos=[]
-for i in range(0,16):
-	photos.append(PhotoImage(file="./images/"+str(i)+".png"))
-
-global Lph , LAff
-if ( t==3 ):
-	Lph = photos[0:9]
-else :
-	Lph = photos[0:16]
-
-
-can=Canvas( width=180*t,height=180*t,bg='white')
-can.pack( side =TOP, padx =20, pady =20)
-fenetre['bg']='white'
-fenetre.title (' Taquin resolution IA')
-
-puzzl = Puzzle(board,can,Lph)
-s = Solver(puzzl,fenetre)
-
-
-def solv():
-	global s
-	s =Solver(puzzl,fenetre)
-	s.solve()
-
-def mel():
-	global puzzl
-	puzzl = puzzl.shuffle()
 
 def melanger():
-	print(puzzl.board)
+    global puzzle
+    puzzle = puzzle.shuffle()
+    solver = Solver(puzzle,fenetre)
+    solution_largeur = solver.solve_largeur()
+    solution_profondeur = solver.solve_profondeur()
+    solution_a = solver.solve_a()
+    if len(solution_a) < len(solution_profondeur):
+        if len(solution_a) < len(solution_largeur):
+            solver.aff5(solution_a, i=0)
+        else:
+            solver.aff5(solution_largeur, i=0)
+    else:
+        if len(solution_profondeur) < len(solution_largeur):
+            solver.aff5(solution_profondeur, i=0)
+        else:
+            solver.aff5(solution_largeur, i=0)
 
-LAff = list([0,1,2,3,4,5,6,7,8])
-LAff=[]
-for row in board:
+
+LAff = []
+for row in puzzle.board:
     LAff.extend(row)
 
-menubar = Menu(fenetre)
-menu1 = Menu(menubar, tearoff=0)
-menu1.add_command(label="melanger", command=mel)
-menu1.add_separator()
-menu1.add_command(label="Quitter", command=fenetre.quit)
-menubar.add_cascade(label="melanger", menu=menu1)
 
-menu2 = Menu(menubar, tearoff=0)
-menu2.add_command(label="Recherche en longeur", command=solv)
-menu2.add_command(label="Recherche en largeur", command=solv)
-menu2.add_command(label="A *", command=s.solve)
-menubar.add_cascade(label="Résoudre", menu=menu2)
-fenetre.config(menu=menubar)
-
-Button(text='MEL',command=mel).pack(side=LEFT)
-Button(text='RESO1',command=solv).pack(side=LEFT)
-Button(text='RESO2',command=solv).pack(side=LEFT)
-
-bouton1 = Radiobutton(fenetre, text="3*3", variable=t, value=3)
-bouton2 = Radiobutton(fenetre, text="4*4", variable=t, value=4)
-bouton1.pack()
-bouton2.pack()
+Button(text='melanger', command=melanger).pack(side=LEFT)
 
 
-for k in range(len(Lph)) :
-    eff = can.create_image((30+ 150*(k % t)), 30+(150*( k // t)), anchor=NW, image=Lph[0])
-    aff = can.create_image((30+ 150*(k % t)), 30+(150*( k // t)), anchor=NW ,image = Lph[LAff[k]])
+for k in range(len(Lph)):
+    eff = can.create_image((30 + 150 * (k % 3)), 30 + (150 * (k // 3)), anchor=NW, image=Lph[0])
+    aff = can.create_image((30 + 150 * (k % 3)), 30 + (150 * (k // 3)), anchor=NW, image=Lph[LAff[k]])
 
 can.pack()
 
